3e12b54ec1105058fe1b884546d2cdb2a4573a52
Jordan <jconheasman@gmail.com>
2020-12-22 18:10:03 -0800

    still issues with the nav bar.

--- resources/main.js
+++ resources/main.js
@@ -5,9 +5,23 @@ const grandparent = document.querySelector(".continer-lg");
 // }
 
 grandparent.addEventListener("click", e => {
-  console.log(e.target);
+  console.log(e);
 });
 
 // $('.hamburger').on('click', function() {
 //   $('.dropdown-nav').toggleclass('active');
 // });
+// function myFunction(){
+//   let doohickey = document.getElementsByClassName('dropdown-nav');
+//   doohickey.setAttribute("class", "dropdown-nav-active");
+//   doohickey.setAttribute("disabled", "");
+
+// }
+var x = document.getElementById("test").nodeType;
+console.log(x);
+
+function myFunction(){
+  var el = document.getElementById("test");
+  el.setAttribute("class", "dropdown-nav-active");
+  el.setAttribute("disabled", "");
+}
