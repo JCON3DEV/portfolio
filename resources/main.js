2ef2ed4977300d481084c169afe89b5ed8d85865
Jordan <jconheasman@gmail.com>
2021-01-08 09:51:28 -0800

    created the toggle slider for navigation. specific ids added to the navbar

--- resources/main.js
+++ resources/main.js
@@ -1,12 +1,25 @@
-const grandparent = document.querySelector(".continer-lg");
+const hamburger = document.querySelector(".hamburger");
 
 // function e() {
 
 // }
 
-grandparent.addEventListener("click", e => {
-  console.log(e);
+hamburger.addEventListener("click", (e) => {
+  console.log("This is e; ", e);
+  // let doohickey = document.getElementsByClassName("main-navigation");
+  // console.log("What is doohickey? ",doohickey);
+  // doohickey[0].setAttribute("class", "main-navigation extended");
+
+  var toggle = document.getElementById("toggle");
+
+  if (toggle.classList.contains("extended")) {
+    toggle.classList.remove("extended");
+  } else {
+    toggle.classList.add("extended");
+  }
+
 });
+
 
 // $('.hamburger').on('click', function() {
 //   $('.dropdown-nav').toggleclass('active');
@@ -17,11 +30,13 @@ grandparent.addEventListener("click", e => {
 //   doohickey.setAttribute("disabled", "");
 
 // }
-var x = document.getElementById("test").nodeType;
-console.log(x);
 
-function myFunction(){
-  var el = document.getElementById("test");
-  el.setAttribute("class", "dropdown-nav-active");
-  el.setAttribute("disabled", "");
-}
+// =============== Temp delete ===
+// var x = document.getElementById("test");
+// console.log("This is x; ", x);
+
+// function myFunction(){
+//   var el = document.getElementById("test");
+//   el.setAttribute("class", "dropdown-nav-active");
+//   el.setAttribute("disabled", "");
+// }
